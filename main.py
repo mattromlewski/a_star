f450f8075898eb2bdfd0880217a56e207adc84e8
mattromlewski <maromlew@edu.uwaterloo.ca>
2020-10-19 19:21:16 -0400

    [fix] streamlined main.py

--- main.py
+++ main.py
@@ -10,16 +10,15 @@ if __name__ == "__main__":
     for run, (start, end) in enumerate(zip(startingPoints, endingPoints)):
         maze = Maze(start=start, end=end)
         agent = Agent()
-        # BFS
-        BFSResults = agent.search(maze, 'BFS')
-        caption = "BFS Run {} - {}->{} - Expanded {} nodes - Cost {}".format(run+1, start, end, BFSResults['numExpanded'], BFSResults['cost'])
-        solutionIllustrator.drawMaze(25,25, walls=maze.getWalls(), expandedNodes=BFSResults['closedSet'], path=BFSResults['path'], caption=caption)
-        print("BFS Run {} - Path {}".format(run+1, BFSResults['path']))
-        #DFS
-        DFSResults = agent.search(maze, 'DFS')
-        caption = "DFS Run {} - {}->{} - Expanded {} nodes - Cost {}".format(run+1, start, end, DFSResults['numExpanded'], DFSResults['cost'])
-        solutionIllustrator.drawMaze(25,25, walls=maze.getWalls(), expandedNodes=DFSResults['closedSet'], path=DFSResults['path'], caption=caption)
-
-
-
-
+        for algorithm in ['BFS', 'DFS', 'A*']:
+            results = agent.search(maze, '{}'.format(algorithm))
+            caption = "{} Run {} - {}->{} - Expanded {} nodes - Cost {}"\
+                .format(algorithm, run+1, start, end, results['numExpanded'], results['cost'])
+            solutionIllustrator.drawMaze(
+                25,
+                25, 
+                walls=maze.getWalls(), 
+                expandedNodes=results['closedSet'], 
+                path=results['path'], caption=caption
+            )
+            print("{} Run {} - Path {}".format(algorithm, run+1, results['path']))
